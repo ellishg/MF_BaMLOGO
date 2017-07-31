bb4ca06f835377665b8fa718a75940a29a3c9adf
Ellis Hoag <eshoag2@illinois.edu>
2017-07-31 13:57:53 -0700

    Restructure code

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mfbamlogo',
+      author='Ellis Hoag',
+      packages=['mfbamlogo'],
+      license='BSD',
+      install_requires=['numpy', 'scipy>=0.18'])
